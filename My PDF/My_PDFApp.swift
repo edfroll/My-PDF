9bc6608f00f38f5df3ac158bff4c69d31c26a81d
Edward <edfroll@icloud.com>
2025-12-04 18:31:01 +0300

    Initial Commit

---
+++ My PDF/My_PDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_PDFApp.swift
+//  My PDF
+//
+//  Created by Эдвард on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct My_PDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My PDFTests/My_PDFTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_PDFTests.swift
+//  My PDFTests
+//
+//  Created by Эдвард on 04.12.2025.
+//
+
+import Testing
+@testable import My_PDF
+
+struct My_PDFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
